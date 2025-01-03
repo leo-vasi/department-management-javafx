abe85fa4074ee245a6a7fb40b16126b8211090e6
Leonardo Silva <levasi.leonardo55@gmail.com>
2025-01-03 14:52:43 -0300

    Main View Controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.leo.application.departmentcrudjavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.leo.application.departmentcrudjavafx to javafx.fxml;
+    opens com.leo.gui to javafx.fxml;
+    opens com.leo.controllers to javafx.fxml;
+
     exports com.leo.application.departmentcrudjavafx;
 }
